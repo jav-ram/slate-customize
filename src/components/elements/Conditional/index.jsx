9a1137563e5a9733ba2557373206b97d33a49b2c
Javier <ram16230@uvg.edu.gt>
2021-01-07 08:36:46 -0600

    new function

--- src/components/elements/Conditional/index.jsx
+++ src/components/elements/Conditional/index.jsx
@@ -8,7 +8,7 @@ import styles from './conditional.module.css';
 
 const name = 'conditional';
 
-const action = (event: SyntheticEvent<HTMLButtonElement>, editor) => {
+const action = (event: SyntheticEvent<HTMLButtonElement>, editor: any) => {
     event.preventDefault();
 
     const list = { type: 'block', element: name, children: [{ text: '' }] }
--- src/components/elements/index.jsx
+++ src/components/elements/index.jsx
@@ -8,3 +8,70 @@ export type ElementDefinition = {
     icon: Function,
     component: Node,
 };
+
+type ActionOptionsType = {
+    match: ?(any) => boolean,
+    split: ?boolean,
+}
+
+export type ActionFunctionType = (
+    event: SyntheticEvent<HTMLButtonElement>,
+    editor: any,
+    name: string,
+    type: string,
+
+    toggle: ?boolean,
+    options: ?ActionOptionsType,
+) => void;
+
+export type ActionDefinition = {
+
+    before: ?(SyntheticEvent<HTMLButtonElement>, any) => void,
+    after: ?(SyntheticEvent<HTMLButtonElement>, any) => void,
+
+    split: ?boolean,
+    match: ?(any) => boolean,
+
+}
+
+type actionDefinition = {
+    name: string,
+    editor: any,
+    event: SyntheticEvent<HTMLButtonElement>,
+    type: 'block' | 'inline',
+    isNested: ?boolean,
+    preventDefault: ?boolean,
+
+    set: ?ActionFunctionType,
+    unset: ?ActionFunctionType,
+}
+
+export const handlerMaker = ({
+    name,
+    editor,
+    event,
+    type,
+    isNested=false,
+    preventDefault=true,
+    set,
+    unset
+}: actionDefinition): ((SyntheticEvent<HTMLButtonElement>, any) => void) => {
+    preventDefault && event.preventDefault();
+
+    let action;
+    if (isNested) {
+        action = (event, editor) => {
+            if (set)
+                set(event, editor, name, type, true);
+        }
+    } else {
+        action = (event, editor) => {
+            if (set)
+                set(event, editor, name, type, true);
+            if (unset)
+                unset(event, editor, name, type, true);
+        }
+    }
+
+    return action;
+};
