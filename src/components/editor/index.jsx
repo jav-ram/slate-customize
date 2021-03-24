e60f2f02a8eef65cc48de57fdb27c72739565eb5
javier <ram16230>
2021-03-24 16:33:10 -0600

    cleaning

--- src/components/editor/index.jsx
+++ src/components/editor/index.jsx
@@ -43,10 +43,6 @@ const EditorElement = () => {
                 onChange={value => {
                     setValue(value);
                     // Save the value to Local Storage.
-                    console.log("-----------")
-                    console.log(value);
-                    console.log(editor.history)
-                    console.log("-----------")
                 }}
             >
                 {/* <HoveringToolbar value={value} /> */}
